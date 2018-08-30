a8a24687a786a7364e7856fc7898ce498c095517
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-08-30 21:11:01 +0530

    in the given timeframe adds the blocked websites to the hosts file

--- website_blocker.py
+++ website_blocker.py
@@ -1,6 +1,8 @@
 import time
 from datetime import datetime as dt
+
 hosts_path = r'/private/etc/hosts'
+hosts_temp = 'hosts'
 redirect = '127.0.0.1'
 websites_list = ['www.facebook.com', 'www.instagram.com', 'facebook.com', 'instagram.com']
 
@@ -8,6 +10,13 @@ while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() \
             < dt(dt.now().year, dt.now().month, dt.now().day, 18):
         print("work hours")
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            for website in websites_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + ' ' + website + '\n')
     else:
         print("non work hours")
     time.sleep(5)
